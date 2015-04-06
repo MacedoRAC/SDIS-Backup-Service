18939096bcd3a403f5b3bc87e885b425fa7f13bf
PedroFaria <pedrommlcfaria@gmail.com>
2015-04-06 01:54:27 +0100

    delete protocol started

--- src/protocols/Delete.java
+++ src/protocols/Delete.java
@@ -8,12 +8,11 @@ import main.Main;
 public class Delete extends Thread{
 	private static Communication com;
 
-	public Delete() {
-		String sendMsg = "DELETE " + header[2] + " " + header[3] + Main.getCRLF().toString() + Main.getCRLF().toString();
-
-        
-
+	public Delete(String fileID) {
+		String sendMsg = "DELETE " + fileID + Main.getCRLF().toString() + Main.getCRLF().toString();
         com.send(sendMsg.getBytes(StandardCharsets.ISO_8859_1));
+        
+        System.out.println("Delete message sent!");
 	}
 
 }
